375d61ca4b7f28812e96c7c41e4d32eb64124eb4
D4Will <willydouglass@icloud.com>
2024-07-17 14:26:36 -0500

    added unique constraint to Venue

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from app import db
 class Venue(db.Model):
     __tablename__ = 'Venue'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, unique=True)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
