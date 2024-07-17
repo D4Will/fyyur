70c2eef6729734af8c569e78eb42e8caf6e1e448
D4Will <willydouglass@icloud.com>
2024-07-17 14:20:25 -0500

    commit

---
+++ models.py
@@ -0,0 +1,43 @@
+from app import db
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    need_talent = db.Column(db.Boolean)
+    description = db.Column(db.String(500))
+    #artists = db.relationship('Artist', secondary=Show, backref=db.backref('venues', lazy=True))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    need_talent = db.Column(db.Boolean)
+    description = db.Column(db.String(500))
+    
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+    time = db.Column(db.DateTime)
+    venue = db.relationship(Venue, backref = db.backref('shows'))
+    artist = db.relationship(Artist, backref = db.backref('shows'))
